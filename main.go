19cb6da164f4b37d6df459f683848ac266035d0c
Jeff Brown <brownj@objectcomputing.com>
2025-01-22 08:22:02 -0600

    introduce server package

--- main.go
+++ main.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/jeffscottbrown/applemusic/controllers"
+	"github.com/jeffscottbrown/applemusic/server"
 )
 
 func main() {
-	r := gin.Default()
-
-	r.GET("/search/:term", controllers.Search)
-
-	r.Run()
+	server.RunServer()
 }
